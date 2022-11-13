fba0a6f6bd3c3894f74ad449c0b9af94a34a4d8b
BohdanRysakoVV <bohdan.rysakov@gmail.com>
2022-11-13 05:47:25 +0300

    first week

--- src/main/java/Task2/TestTask2.java
+++ src/main/java/Task2/TestTask2.java
@@ -2,12 +2,9 @@ package Task2;
 
 import org.junit.Assert;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static org.junit.Assert.assertArrayEquals;
 
 public class TestTask2 {
     private final Task2 task2 = new Task2();
